edabc2b602b567923a722a632b70a0b388127358
Salman <salmanmalik508@gmail.com>
2022-08-23 14:09:06 +0500

    ïnitial commit

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCahDxtKokwGfJQj2thaIDAzTwu4vOo9qw",
+  authDomain: "vue-chat-app-c2f1e.firebaseapp.com",
+  projectId: "vue-chat-app-c2f1e",
+  storageBucket: "vue-chat-app-c2f1e.appspot.com",
+  messagingSenderId: "666109066754",
+  appId: "1:666109066754:web:281df28f624728398db11b",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
